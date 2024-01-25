647b26ce6c40be257020ce628776cc9e7f59a7a2
shabeeb942 <shabeebsha942@gmail.com>
2024-01-25 09:30:48 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,595 @@
+/***************************************************
+==================== JS INDEX ======================
+****************************************************
+01. PreLoader Js
+02. Mobile Menu Js
+03. Sidebar Js
+04. Cart Toggle Js
+05. Search Js
+06. Sticky Header Js
+07. Data Background Js
+08. Testimonial Slider Js
+09. Slider Js (Home 3)
+10. Brand Js
+11. Tesimonial Js
+12. Course Slider Js
+13. Masonary Js
+14. Wow Js
+15. Data width Js
+16. Cart Quantity Js
+17. Show Login Toggle Js
+18. Show Coupon Toggle Js
+19. Create An Account Toggle Js
+20. Shipping Box Toggle Js
+21. Counter Js
+22. Parallax Js
+23. InHover Active Js
+
+****************************************************/
+
+(function ($) {
+"use strict";
+
+	var windowOn = $(window);
+	////////////////////////////////////////////////////
+    // 01. PreLoader Js
+	windowOn.on('load',function() {
+		$("#loading").fadeOut(500);
+	});
+
+	////////////////////////////////////////////////////
+    // 02. Mobile Menu Js
+	$('#mobile-menu').meanmenu({
+		meanMenuContainer: '.mobile-menu',
+		meanScreenWidth: "1199",
+		meanExpand: ['<i class="fal fa-plus"></i>'],
+	});
+
+
+	////////////////////////////////////////////////////
+    // 03. Sidebar Js
+	$("#sidebar-toggle").on("click", function () {
+		$(".sidebar__area").addClass("sidebar-opened");
+		$(".body-overlay").addClass("opened");
+	});
+	$(".sidebar__close-btn").on("click", function () {
+		$(".sidebar__area").removeClass("sidebar-opened");
+		$(".body-overlay").removeClass("opened");
+	});
+
+
+	////////////////////////////////////////////////////
+    // 04. Cart Toggle Js
+	$(".cart-toggle-btn").on("click", function () {
+		$(".cartmini__wrapper").addClass("opened");
+		$(".body-overlay").addClass("opened");
+	});
+	$(".cartmini__close-btn").on("click", function () {
+		$(".cartmini__wrapper").removeClass("opened");
+		$(".body-overlay").removeClass("opened");
+	});
+	$(".body-overlay").on("click", function () {
+		$(".cartmini__wrapper").removeClass("opened");
+		$(".sidebar__area").removeClass("sidebar-opened");
+		$(".header__search-3").removeClass("search-opened");
+		$(".body-overlay").removeClass("opened");
+	});
+
+
+	////////////////////////////////////////////////////
+    // 05. Search Js
+	$(".search-toggle").on("click", function () {
+		$(".header__search-3").addClass("search-opened");
+		$(".body-overlay").addClass("opened");
+	});
+	$(".header__search-3-btn-close").on("click", function () {
+		$(".header__search-3").removeClass("search-opened");
+		$(".body-overlay").removeClass("opened");
+	});
+
+
+	////////////////////////////////////////////////////
+    // 06. Sticky Header Js
+	windowOn.on('scroll', function () {
+		var scroll = $(window).scrollTop();
+		if (scroll < 100) {
+			$("#header-sticky").removeClass("sticky");
+		} else {
+			$("#header-sticky").addClass("sticky");
+		}
+	});
+
+	////////////////////////////////////////////////////
+    // 07. Data Background Js
+	$("[data-background").each(function () {
+		$(this).css("background-image", "url( " + $(this).attr("data-background") + "  )");
+	});
+
+  
+	////////////////////////////////////////////////////
+    // 08. Testimonial Slider Js
+	var swiper = new Swiper('.testimonial__slider', {
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+	
+
+
+	////////////////////////////////////////////////////
+    // 09. Slider Js (Home 3)
+	var galleryThumbs = new Swiper('.slider__nav', {
+		spaceBetween: 0,
+		slidesPerView: 4,
+		freeMode: true,
+		watchSlidesVisibility: true,
+		watchSlidesProgress: true,
+		
+	});
+	var galleryTop = new Swiper('.slider__wrapper', {
+		spaceBetween: 0,
+		effect: 'fade',
+		loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		thumbs: {
+			swiper: galleryThumbs
+		}
+	});
+
+
+	////////////////////////////////////////////////////
+    // 10. Brand Js
+	var swiper = new Swiper('.brand__slider', {
+		slidesPerView: 6,
+		spaceBetween: 30,
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		breakpoints: {
+			'1200': {
+				slidesPerView: 6,
+			},
+			'992': {
+				slidesPerView: 5,
+			},
+			'768': {
+				slidesPerView: 3,
+			},
+			'576': {
+				slidesPerView: 2,
+			},
+			'0': {
+				slidesPerView: 1,
+			},
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+
+
+	////////////////////////////////////////////////////
+    // 11. Tesimonial Js
+	var tesimonialThumb = new Swiper('.testimonial-nav', {
+		spaceBetween: 20,
+		slidesPerView: 3,
+		loop: true,
+		freeMode: true,
+		loopedSlides: 3, //looped slides should be the same
+		watchSlidesVisibility: true,
+		watchSlidesProgress: true,
+		centeredSlides: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+	  });
+	var testimonialText = new Swiper('.testimonial-text', {
+	spaceBetween: 0,
+	loop: true,
+	loopedSlides: 5, //looped slides should be the same
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	thumbs: {
+		swiper: tesimonialThumb,
+	},
+	});
+
+	////////////////////////////////////////////////////
+   	// 12. Course Slider Js
+	var swiper = new Swiper('.course__slider', {
+	spaceBetween: 30,
+	slidesPerView: 2,
+	breakpoints: {  
+		'768': {
+			slidesPerView: 2,
+		},
+		'576': {
+			slidesPerView: 1,
+		},
+		'0': {
+			slidesPerView: 1,
+		},
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	});
+	
+	////////////////////////////////////////////////////
+    // 13. Masonary Js
+	$('.grid').imagesLoaded( function() {
+		// init Isotope
+		var $grid = $('.grid').isotope({
+		  itemSelector: '.grid-item',
+		  percentPosition: true,
+		  masonry: {
+			// use outer width of grid-sizer for columnWidth
+			columnWidth: '.grid-item',
+		  }
+		});
+
+
+	// filter items on button click
+	$('.masonary-menu').on( 'click', 'button', function() {
+	  var filterValue = $(this).attr('data-filter');
+	  $grid.isotope({ filter: filterValue });
+	});
+
+	//for menu active class
+	$('.masonary-menu button').on('click', function(event) {
+		$(this).siblings('.active').removeClass('active');
+		$(this).addClass('active');
+		event.preventDefault();
+	});
+
+	});
+
+
+	////////////////////////////////////////////////////
+    // 14. Wow Js
+	new WOW().init();
+
+	////////////////////////////////////////////////////
+    // 15. Data width Js
+	$("[data-width]").each(function () {
+		$(this).css("width", $(this).attr("data-width"));
+	  });
+	
+
+	////////////////////////////////////////////////////
+    // 16. Cart Quantity Js
+	$('.cart-minus').click(function () {
+		var $input = $(this).parent().find('input');
+		var count = parseInt($input.val()) - 1;
+		count = count < 1 ? 1 : count;
+		$input.val(count);
+		$input.change();
+		return false;
+	});
+	$('.cart-plus').click(function () {
+		var $input = $(this).parent().find('input');
+		$input.val(parseInt($input.val()) + 1);
+		$input.change();
+		return false;
+	});
+
+
+
+
+	////////////////////////////////////////////////////
+	// 17. Show Login Toggle Js
+	$('#showlogin').on('click', function () {
+		$('#checkout-login').slideToggle(900);
+	});
+
+	////////////////////////////////////////////////////
+	// 18. Show Coupon Toggle Js
+	$('#showcoupon').on('click', function () {
+		$('#checkout_coupon').slideToggle(900);
+	});
+
+	////////////////////////////////////////////////////
+	// 19. Create An Account Toggle Js
+	$('#cbox').on('click', function () {
+		$('#cbox_info').slideToggle(900);
+	});
+
+	////////////////////////////////////////////////////
+	// 20. Shipping Box Toggle Js
+	$('#ship-box').on('click', function () {
+		$('#ship-box-info').slideToggle(1000);
+	});
+
+	////////////////////////////////////////////////////
+	// 21. Counter Js
+	new PureCounter();
+	new PureCounter({
+		filesizing: true,
+		selector: ".filesizecount",
+		pulse: 2,
+	});
+	
+	////////////////////////////////////////////////////
+	// 22. Parallax Js
+	if ($('.scene').length > 0 ) {
+		$('.scene').parallax({
+			scalarX: 10.0,
+			scalarY: 15.0,
+		}); 
+	};
+
+	////////////////////////////////////////////////////
+    // 23. InHover Active Js
+	$('.hover__active').on('mouseenter', function () {
+		$(this).addClass('active').parent().siblings().find('.hover__active').removeClass('active');
+	});
+
+})(jQuery);
+
+
+// new
+
+
+
+
+var autoplayIntervalInSeconds = .5;
+
+
+class PostSlider {
+
+    constructor(containerElement,autoplayIntervalInSeconds) {
+        this.container = containerElement;
+        if (!this.container) {
+            throw new Error(`Container not found.`);
+        }
+
+        this.slider = this.container.querySelector('.slider');
+        this.prevBtn = this.container.querySelector('.handles .prev');
+        this.nextBtn = this.container.querySelector('.handles .next');
+
+        this.sLiderWidth = this.slider.clientWidth;
+        this.oneSLideWidth = this.container.querySelector('.slide:nth-child(2)').clientWidth;
+        console.log(this.oneSLideWidth);
+        this.sildesPerPage = Math.trunc(this.sLiderWidth / this.oneSLideWidth);
+        this.slideMargin = ((this.sLiderWidth - (this.sildesPerPage * this.oneSLideWidth)) / (this.sildesPerPage * 2)).toFixed(5);
+        this.changeSlidesMargins();
+
+        // Assign this.dots before calling bindDotClickHandlers
+        this.dots = this.container.querySelectorAll('.dots span');
+        this.bindDotClickHandlers();
+
+        this.makeSliderScrollable();
+        this.prevBtn.addEventListener('click', () => this.prevSlider());
+        this.nextBtn.addEventListener('click', () => this.nextSlider());
+
+        this.createDots();
+        this.setActiveDotByScroll();
+
+        this.autoplayInterval = null;
+        this.autoplayDelay = autoplayIntervalInSeconds*1000;
+
+        this.startAutoplay()
+        this.container.addEventListener('mouseenter', () => this.pauseAutoplay());
+        this.container.addEventListener('mouseleave', () => this.startAutoplay());
+
+        return this;
+    }
+    changeSlidesMargins() {
+        const slides = this.container.querySelectorAll('.slide');
+        if (this.oneSLideWidth*2 > this.sLiderWidth){
+            this.slideMargin = 1;
+            this.oneSLideWidth = this.oneSLideWidth + (this.sLiderWidth - this.oneSLideWidth - 2);
+            slides.forEach(slide => {
+                slide.style.margin = "0 " + this.slideMargin + "px";
+                slide.style.minWidth = this.oneSLideWidth + "px";
+            });
+
+        }else {
+            slides.forEach(slide => {
+                slide.style.margin = "0 " + this.slideMargin + "px";
+            });
+        }
+    }
+
+
+    scrollToPosition(position, smooth =true) {
+        console.log('Scrolling to position:', position);
+        const currentPosition = this.slider.scrollLeft;
+        const newPosition = currentPosition + position;
+
+        this.slider.scrollTo({
+            top: 0,
+            left: newPosition,
+            behavior: smooth ? 'smooth' : 'instant'
+        });
+
+        console.log('Current position - New position:', currentPosition - newPosition);
+
+        setTimeout(() => {
+            this.snapToNearestSlide();
+        }, 300);
+    }
+    scrollWithDots(pos) {
+        this.slider.scrollTo({
+            top: 0,
+            left: pos,
+            behavior: "smooth"
+        });
+    }
+
+    handleDotClick(index) {
+        const position = index * (this.slider.getBoundingClientRect()['width']);
+        this.scrollWithDots(position);
+    }
+
+    changeActiveDot(i) {
+        for (let j = 0; j < this.dots.length; j++) {
+            this.dots[j].classList.remove('active');
+        }
+        this.dots[i].classList.add('active');
+    }
+
+
+    bindDotClickHandlers() {
+        for (let i = 0; i < this.dots.length; i++) {
+            this.dots[i].addEventListener('click', () => {
+                console.log('Dot clicked:', i);
+                this.handleDotClick(i);
+            });
+        }
+    }
+    snapToNearestSlide(){
+
+        const currentPosition = this.slider.scrollLeft;
+        const nearestLeftScroll = Math.round(currentPosition / (this.oneSLideWidth+(this.slideMargin*2))) * (this.oneSLideWidth+(this.slideMargin*2));
+        console.log(nearestLeftScroll);
+        this.slider.scrollTo({
+            left:  nearestLeftScroll,
+            behavior: 'smooth'
+        });
+    }
+    makeSliderScrollable() {
+        let isDragging = false;
+        let startPosition;
+        let startScrollPosition;
+
+        this.slider.addEventListener('mousedown', (event) => startDrag(event));
+        this.slider.addEventListener('touchstart', (event) => startDrag(event));
+
+        const startDrag = (event) => {
+            isDragging = true;
+            startPosition = event.clientX || event.touches[0].clientX;
+            startScrollPosition = this.slider.scrollLeft;
+
+            document.addEventListener('mousemove', drag);
+            document.addEventListener('touchmove', drag);
+            document.addEventListener('mouseup', endDrag);
+            document.addEventListener('touchend', endDrag);
+        };
+
+        const drag = (event) => {
+            if (isDragging) {
+                const currentX = event.clientX || event.touches[0].clientX;
+                const deltaX = currentX - startPosition;
+                this.slider.scrollLeft = startScrollPosition - deltaX;
+            }
+        };
+
+        const endDrag = () => {
+            if (isDragging) {
+                isDragging = false;
+                const currentPosition = this.slider.scrollLeft;
+                const nearestLeftScroll = Math.round(currentPosition / (this.oneSLideWidth+(this.slideMargin*2))) * (this.oneSLideWidth+(this.slideMargin*2));
+                console.log(nearestLeftScroll);
+                this.slider.scrollTo({
+                    left:  nearestLeftScroll,
+                    behavior: 'smooth'
+                });
+
+                document.removeEventListener('mousemove', drag);
+                document.removeEventListener('touchmove', drag);
+                document.removeEventListener('mouseup', endDrag);
+                document.removeEventListener('touchend', endDrag);
+            }
+        };
+    }
+
+    setActiveDotByScroll() {
+        this.dots = this.container.querySelectorAll('.dots span');
+        this.slider.addEventListener('scroll', () => {
+            const scrollLeft = this.slider.scrollLeft;
+            const currentIndex = Math.trunc((Math.abs(scrollLeft) + 2) / this.slider.clientWidth);
+
+            console.log('Scroll Left:', scrollLeft);
+            console.log('Current Index:', currentIndex);
+
+            for (let i = 0; i < this.dots.length; i++) {
+                this.dots[i].classList.remove('active');
+            }
+
+            this.dots[currentIndex].classList.add('active');
+
+            this.prevBtn.style.opacity = Math.abs(scrollLeft) < 1 ? '0' : '1'; /*it means there is no element before so it would hide prev button*/
+            this.nextBtn.style.opacity = Math.abs(scrollLeft) + 2 >= this.slider.scrollWidth - this.slider.clientWidth ? '0' : '1'; /*it means there is no element after so it would hide next button*/
+        });
+    }
+
+
+    nextSlider() {
+        const totalWidth = this.slider.scrollWidth;
+        const currentScroll = this.slider.scrollLeft;
+        const nextScroll = currentScroll + this.oneSLideWidth + (this.slideMargin * 2);
+
+        if (nextScroll + this.slider.clientWidth > totalWidth) {
+            // If next slide goes beyond the end, scroll to the beginning
+            this.slider.scrollTo({
+                left: 0,
+                behavior: 'smooth'
+            });
+        } else {
+            this.scrollToPosition(this.oneSLideWidth + (this.slideMargin * 2));
+        }
+    }
+
+    prevSlider() {
+        const currentScroll = this.slider.scrollLeft;
+        const prevScroll = currentScroll - (this.oneSLideWidth + (this.slideMargin * 2));
+
+        if (prevScroll < 0) {
+            // If previous slide goes before the beginning, scroll to the end
+            this.slider.scrollTo({
+                left: this.slider.scrollWidth - this.slider.clientWidth,
+                behavior: 'smooth'
+            });
+        } else {
+            this.scrollToPosition(-1 * (this.oneSLideWidth + (this.slideMargin * 2)));
+        }
+    }
+
+    createDots() {
+        const dotCount = Math.floor(this.slider.scrollWidth / this.slider.clientWidth);
+        const dotsContainer = this.container.querySelector('.dots');
+        dotsContainer.innerHTML = '';
+
+        for (let i = 0; i < dotCount; i++) {
+            const dot = document.createElement('span');
+            dot.addEventListener('click', () => {
+                // this.changeActiveDot(i);
+                this.handleDotClick(i);
+            });
+
+            if (i === 0) {
+                dot.classList.add('active');
+            }
+
+            dotsContainer.appendChild(dot);
+        }
+    }
+
+    startAutoplay() {
+        this.autoplayInterval = setInterval(() => {
+            this.nextSlider();
+        }, this.autoplayDelay);
+    }
+
+    pauseAutoplay() {
+        clearInterval(this.autoplayInterval);
+    }
+}
+
+
+window.addEventListener('load',function (){
+    var container = document.querySelector('.PostSlide .innerContainer');
+    new PostSlider(container,3);
+})
